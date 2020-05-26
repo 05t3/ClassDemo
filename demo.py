779dd9d2c01713140728efaf876d04acbc17fd90
OSTE008TRIAL <58165365+OSTE008TRIAL@users.noreply.github.com>
2020-05-26 22:37:39 +0300

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#run by typing python3 scanner.py on terminal
+#you must install the code below on terminal before running 
+#pip3 install python.nmap
+
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("Welcome, this is a simple nmap automation tool By Stephen")
+print("<----------------------------------------------------->")
+
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+resp = input("""\nPlease enter the type of scan you want to run
+                1)SYN ACK Scan
+                2)UDP Scan\n""")
+print("You have selected option: ", resp)
+
